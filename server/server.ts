b50f3f65c9a844d3a5d028785ec2f32eaa4c3a72
Yasanga32 <yasangashanuka33@gmail.com>
2026-03-25 15:56:39 +0530

    user controllers added

---
+++ server/server.ts
@@ -0,0 +1,45 @@
+import "dotenv/config";
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import { connectDB } from "./configs/db.js";
+import session from 'express-session'
+import MongoStore from 'connect-mongo';
+
+declare module 'express-session'{
+    interface SessionData{
+        isLoggedIn:boolean;
+        userId: string
+    }
+}
+
+await connectDB()
+
+const app = express();
+
+// Middleware
+app.use(cors({
+    origin : ['http://localhost:5173','http://localhost:3000']
+}))
+
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    resave:false,
+    saveUninitialized:false,
+    cookie: {maxAge:1000 * 60 * 60 *24 * 7}, //7 days
+    store: MongoStore.create({
+        mongoUrl:process.env.MONGODB_URL as string,
+        collectionName: 'sessions'
+    })
+}))
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
